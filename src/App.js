cf61dcd7575d041ccca2fa9e5b22de4cc05ceaeb
chrispanz1 <133619340+chrispanz1@users.noreply.github.com>
2023-07-28 11:37:09 -0400

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form';
+import MovieDisplay from './components/MovieDisplay';
+import { useState, useEffect } from 'react';
 
 function App() {
+const apiKey="98e3fb1f"
+
+const [movie, setMovie] = useState("")
+
+
+const getMovie = async(searchTerm)=>{
+  const response = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`)
+
+//--->Go to the site and search for Title
+const data = await response.json()
+//-----Turn info into JSON format
+setMovie(data)
+// ------> Take retrieved data and set it as a current Movie
+}
+
+useEffect(()=>{
+  getMovie("Clueless");
+}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Form movieSearch={getMovie}/>
+
+     <MovieDisplay movie={movie}/>
     </div>
   );
 }
